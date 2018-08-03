25cf2c5954bfd77f86bf8a83de5bef0f197942a1
Ahmed Mohamed Gaber <ahmed.mohamed.gaber@gmail.com>
2018-08-03 05:52:00 +0200

    infowidow edited

--- src/map.js
+++ src/map.js
@@ -15,6 +15,8 @@ class Map extends Component {
       {name: "Abou El Abbas El Morsy Mosque", location: {lat: 31.2057, lng: 29.8824}}
     ],
    markers:[],
+   places: []
+
 }
 
 
@@ -28,18 +30,20 @@ class Map extends Component {
 initMap(){
 const {google} = this.props
 const maps = google.maps;
-  var map = new maps.Map(document.getElementById("map"),{
+var map = new maps.Map(document.getElementById("map"),{
           center: {lat:31.206656, lng:29.893934},
           zoom: 15,
           mapTypeId:"roadmap",
           mapTypeControl: false
         });
-  this.addMarkers(map);
+  this.addMarkers(map,google);
 
  // return map
 }
 
-populateInfoWindow(marker, infowindow, map){
+populateInfoWindow(marker, infowindow, map,service,google){
+	var geocoder = new google.maps.Geocoder();
+	
 	if (infowindow.marker !== marker) {
           infowindow.setContent('');
           infowindow.marker = marker;
@@ -48,14 +52,50 @@ populateInfoWindow(marker, infowindow, map){
           });
     }
     infowindow.setContent('<div>' + marker.title + '</div><div><p>Lat Lng </p>' +marker.position+ '</div>');
-    infowindow.open(map, marker);
+    geocoder.geocode({'location': marker.position}, function(results, status)	 {
+    service.getDetails({
+        placeId: results[0].place_id
+      }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+			let infoContent =''
+		    if (place.name) {
+		            infoContent += '<p>' + place.name + '</p>';
+		          }
+		          if (place.formatted_address) {
+		            infoContent += '<p>' + place.formatted_address+'</p>'
+		          }
+		          if (place.formatted_phone_number) {
+		            infoContent += '<p>' + place.formatted_phone_number+'</p>'
+		          }
+		         /* if (place.opening_hours) {
+		          	console.log(place.opening_hours)
+            infoContent += '<br><br><strong>Hours:</strong><br>' +
+                '<p>'+place.opening_hours.weekday_text[0] + '</p>' +
+                '<p>'+place.opening_hours.weekday_text[1] + '</p>' +
+               '<p>' +place.opening_hours.weekday_text[2] + '</p>' +
+                '<p>'+place.opening_hours.weekday_text[3] + '</p>' +
+                '<p>'+place.opening_hours.weekday_text[4] + '</p>' +
+                '<p>'+place.opening_hours.weekday_text[5] + '</p>' +
+                '<p>'+place.opening_hours.weekday_text[6]+'</p>'
+          }
+          if (place.photos) {
+          			          	console.log(place.photos)
 
+            infoContent += '<br><br><img src="' + place.photos[0].getUrl(
+                {maxHeight: 50, maxWidth: 100}) + '">'
+          }*/
+		  	infowindow.setContent('<div>' + marker.title + '</div><div><p>Lat Lng </p>' +marker.position+ infoContent +'</div>');          
+        }});
+        })
+       
+    infowindow.open(map, marker);
+//console.log(marker)
   }
 
-addMarkers(map){
-    const {google} = this.props
+addMarkers(map,google){
     let bounds = new google.maps.LatLngBounds()
     let Infowindow = new google.maps.InfoWindow();
+    let service = new google.maps.places.PlacesService(map);
 	this.state.locations.forEach((location) => {
 	    let marker = new google.maps.Marker({
 	    position: {lat: location.location.lat, lng: location.location.lng},
@@ -65,7 +105,7 @@ addMarkers(map){
 	  	bounds.extend(location.location);
 	   	this.setState({markers:[...this.state.markers,marker]});
 		marker.addListener('click',()=>(
-		this.populateInfoWindow(marker, Infowindow,map)
+		this.populateInfoWindow(marker, Infowindow,map,service,google)
 		));
 	})
     map.fitBounds(bounds);
