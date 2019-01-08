97a19a149a2b6e4e6bbb52fe8b193bd0aab70284
Ahmed Mohamed Gaber <ahmed.mohamed.gaber@gmail.com>
2019-01-08 23:00:28 +0200

    Updates

---
+++ precache-manifest.cd823aee4f71a6d2d825d53fecb1027b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2730a3512a4067938e0e",
+    "url": "/Google_Map_App/static/js/runtime~main.2730a351.js"
+  },
+  {
+    "revision": "e06052743dfd1d61b29f",
+    "url": "/Google_Map_App/static/js/main.e0605274.chunk.js"
+  },
+  {
+    "revision": "6ca94298caa2543b60f7",
+    "url": "/Google_Map_App/static/js/1.6ca94298.chunk.js"
+  },
+  {
+    "revision": "e06052743dfd1d61b29f",
+    "url": "/Google_Map_App/static/css/main.f36f04ee.chunk.css"
+  },
+  {
+    "revision": "173a82488d4e272bf579dcbdb45aa62e",
+    "url": "/Google_Map_App/index.html"
+  }
+];
